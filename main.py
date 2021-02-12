53bb7aa531765b2b8f699b599a39bca01a0277fd
Shane McDonough <shanemcdman@gmail.com>
2021-02-11 17:15:22 -0800

    Add a todo comment

--- main.py
+++ main.py
@@ -25,6 +25,7 @@ def perform_op(num1, operator, num2):
         raise 'Invalid operator'
 
 def calc_simple_string(s: str) -> int:
+    # TODO: Order of operations
     equation = re.findall(r'\d+(?:\.\d+)?|[+*/\-]',s)
     while len(equation) > 1:
         equation.insert(0, perform_op(float(equation.pop(0)), equation.pop(0), float(equation.pop(0))))
