7471f2782c4bb3d295af0e8a6b64c818307f3264
Shane McDonough <shanemcdman@gmail.com>
2021-03-30 19:50:29 -0700

    Update simplify for power

--- tree_method.py
+++ tree_method.py
@@ -8,7 +8,8 @@ class BinaryTreeNodeBase:
 class BinaryOperatorBase(BinaryTreeNodeBase):
     op = lambda self, x, y: x
     op_char = '_'
-    def simplify(self): pass
+    def simplify(self):
+        return self
     def exec(self):
         left = self.left.exec() if isinstance(self.left, BinaryOperatorBase) else self.left
         right = self.right.exec() if isinstance(self.right, BinaryOperatorBase) else self.right
@@ -32,9 +33,7 @@ class Minus(BinaryOperatorBase):
     op = lambda self, x, y: x - y
     op_char = '-'
     def simplify(self):
-        if isinstance(self.left, Const) and self.left.val == 0:
-            return -self.right
-        elif isinstance(self.right, Const) and self.right.val == 0:
+        if isinstance(self.right, Const) and self.right.val == 0:
             return self.left
         return self
 
@@ -70,6 +69,13 @@ class Divide(BinaryOperatorBase):
 class Power(BinaryOperatorBase):
     op = lambda self, x, y: x ** y
     op_char = '^'
+    def simplify(self):
+        if isinstance(self.right, Const):
+            if self.right.val == 0:
+                return Const(1)
+            elif self.right.val == 1:
+                return self.left
+        return self
 
 @dataclass
 class Const:
@@ -87,7 +93,10 @@ class Var:
 
 if __name__ == '__main__':
     eq = Minus(Plus(Const(10), Const(5)), Times(Const(1), Var('x')))
-    print(eq)
-    print(eq.exec())
+    print(eq, ' = ', eq.exec())
     eq = Times(Minus(Const(32), Const(31)), Var('x'))
-    print(eq.exec())
+    print(eq, ' = ', eq.exec())
+    eq = Power(Const(2), Const(8))
+    print(eq, ' = ', eq.exec())
+    eq = Power(Var('x'), Const(0))
+    print(eq, ' = ', eq.exec())
